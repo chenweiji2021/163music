88409998af62074981b4955c1757e81fb217792b
wiki <3057261602@qq.com>
2021-09-24 23:16:49 +0800

    Revert "del222"
    This reverts commit 9d907edeab13ea9af49f4174882c5d17f54500be.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "@/assets/css/reset.css";
 import App from "./App";
 
-console.log("GitHub action111");
+console.log("项目已启动");
+console.log("test git");
+console.log("test pull");
 
 ReactDOM.render(<App />, document.getElementById("root"));
